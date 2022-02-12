1c6d55182d70273c02b1613ac1980445e8deb840
devlse <59230@naver.com>
2022-02-13 01:07:20 +0900

    ClickToEdit 기능 구현

--- src/component/ClickToEdit.js
+++ src/component/ClickToEdit.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import '../styles/common.css';
 
@@ -18,7 +18,7 @@ const EditContainer = styled.div`
             border: 1px solid hsl(150deg 3% 88%);
             padding: 0 7px;
             border-radius: 3px;
-
+            text-align: center;
         }
     }
 
@@ -30,14 +30,29 @@ const EditContainer = styled.div`
 `
 
 function ClickToEdit () {
+    const [inputs, setInputs] = useState({
+        name: '김코딩',
+        age: '20'
+    });
+
+    const { name, age } = inputs;
+
+    const onChange = (e) => {
+        const { name, value } = e.target; // input의 name 값을 참조 (e.target.name, e.target.value)
+        setInputs({
+            ...inputs,     // 기존의 input 객체를 복사한 뒤
+            [name]: value // name 키를 가진 값을 value로 설정
+        });
+    };
+
     return (
         <>
             <div className='container'>
                 <div className="common-title">ClickToEdit</div>
                     <EditContainer>
-                        <div className="editItem">이름 <input type="text" /></div>
-                        <div className="editItem">나이 <input type="text" /></div>
-                        <span className="editInform">이름: 최해커 / 나이: 20</span>
+                        <div className="editItem">이름 <input name="name" type="text" onChange={onChange} value={name}/></div>
+                        <div className="editItem">나이 <input name="age" type="text" onChange={onChange} value={age}/></div>
+                        <span className="editInform">이름: {name} / 나이: {age}</span>
                     </EditContainer>
             </div>
         </>
